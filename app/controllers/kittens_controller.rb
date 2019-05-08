7642c4586269abf9de6e14bb1d2d5a18f81aa025
Alex <nikitin2009@gmail.com>
2019-05-08 18:47:39 +0300

    Add Kitten controller actions, implement :index and :show

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,4 +1,34 @@
 class KittensController < ApplicationController
+  before_action :find_kitten, only: [:show, :edit, :update]
+
   def index
+    @kittens = Kitten.all
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(:kitten_params)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+private
+
+  def kitten_params
+    params.require[:kitten].permit(:name, :age, :cuteness, :softness)
+  end
+
+  def find_kitten
+    @kitten = Kitten.find_by(id: params[:id])
   end
 end
